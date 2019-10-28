d8d26da6850fe1d02f42f7f9567604083e8eeb56
Isaac Waide <waidei@hartwick.edu>
2019-10-28 11:39:08 -0400

    Update 27, 2019

--- Craps.py
+++ Craps.py
@@ -3,12 +3,12 @@
 # Craps Due Friday Oct 25,2019
 import random
 
-bank = create_bank
+bank = account
 roll = roll_dice
 bet = bank
 
 
-def create_bank():
+def account():
     print("Welcome to Hartwick College roll of chance.")
 
 
@@ -23,5 +23,7 @@ while bank > 0 and bet > 0:
     def roll_dice():
         return random.randint(2, 12)
 print(f"No more bets, time to roll the two dice, you rolled a {roll}.")
+if roll == 7 or roll == 11:
+    print("Congrats Champ! You won!")
 
 choice = input()
